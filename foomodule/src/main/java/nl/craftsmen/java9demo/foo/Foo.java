35984d9aeca53a26e855a7add2b4b6b9d8b2064b
Michel Schudel <michel.schudel@gmail.com>
2018-01-03 17:02:17 +0100

    Second commit.

--- foomodule/src/main/java/nl/craftsmen/java9demo/foo/Foo.java
+++ foomodule/src/main/java/nl/craftsmen/java9demo/foo/Foo.java
@@ -3,6 +3,10 @@ package nl.craftsmen.java9demo.foo;
 import nl.craftsmen.java9demo.bar.Bar;
 import nl.craftsmen.java9demo.bar.util.BarHelper;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Foo {
 
     private Bar bar;
@@ -19,5 +23,13 @@ public class Foo {
 
     public void execute() {
         bar.execute();
+        barhelper.execute();
+        try {
+            Class<?> barHelperClass = Class.forName("nl.craftsmen.java9demo.bar.util.BarHelper");
+            Method[] methods = barHelperClass.getMethods();
+            System.out.println(Arrays.stream(methods).map(Method::toString).collect(Collectors.toList()));
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
