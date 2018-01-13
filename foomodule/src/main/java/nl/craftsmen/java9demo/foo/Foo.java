4aba55bb213293bc4a743f0fb755f549df993d06
Michel Schudel <michel.schudel@gmail.com>
2018-01-13 19:38:27 +0100

    Changed some deps

--- foomodule/src/main/java/nl/craftsmen/java9demo/foo/Foo.java
+++ foomodule/src/main/java/nl/craftsmen/java9demo/foo/Foo.java
@@ -2,12 +2,12 @@ package nl.craftsmen.java9demo.foo;
 
 import nl.craftsmen.java9demo.bar.Bar;
 
-//import nl.craftsmen.java9demo.bar.util.BarHelper;
+import nl.craftsmen.java9demo.bar.util.BarHelper;
 
 public class Foo {
 
     private Bar bar;
-    //private BarHelper barhelper  = new BarHelper();
+    private BarHelper barhelper  = new BarHelper();
 
     public static void main(String[] args) {
         Foo foo = new Foo();
@@ -20,7 +20,7 @@ public class Foo {
 
     public void execute() {
         bar.execute();
-        //barhelper.execute();
+        barhelper.execute();
         try {
             Class<?> barHelperClass = Class.forName("nl.craftsmen.java9demo.bar.util.BarHelper");
             barHelperClass.getConstructor().newInstance();
